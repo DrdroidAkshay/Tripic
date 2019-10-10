6795c885d9ebac6393d43df5ee845e3d4894994d
DrdroidAkshay <akshayyadav04031998@gmail.com>
2019-10-10 09:43:25 +0530

    Splash screen added

--- app/src/main/java/com/tripic/tripic/MainActivity.java
+++ app/src/main/java/com/tripic/tripic/MainActivity.java
@@ -2,27 +2,38 @@ package com.tripic.tripic;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView img;
-        img=findViewById(R.id.logo1);
-        img.setOnClickListener(new View.OnClickListener() {
+//        ImageView img;
+//        img=findViewById(R.id.logo1);
+//        img.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent=new Intent(MainActivity.this,login.class);
+//                 startActivity(intent);
+//
+//            }
+//        });
+        new Handler().postDelayed(new Runnable(){
             @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this,login.class);
-                 startActivity(intent);
-
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(MainActivity.this,login.class);
+                MainActivity.this.startActivity(mainIntent);
+                MainActivity.this.finish();
             }
-        });
-
+        }, SPLASH_DISPLAY_LENGTH);
 
     }
 }
