8b8d2200e8addcee933caf666a1376dcc1edbcee
DrdroidAkshay <akshayyadav04031998@gmail.com>
2020-04-24 19:35:40 +0530

    My account.java initiated

--- app/src/main/java/com/tripic/tripic/MyAccount.java
+++ app/src/main/java/com/tripic/tripic/MyAccount.java
@@ -3,11 +3,18 @@ package com.tripic.tripic;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mikhaellopez.circularimageview.CircularImageView;
+
 public class MyAccount extends AppCompatActivity {
+
+    CircularImageView profilepic,profilepicchangebtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_account);
+
+        profilepic=findViewById(R.id.img_profile);
+        profilepicchangebtn=findViewById(R.id.img_plus);
     }
 }
