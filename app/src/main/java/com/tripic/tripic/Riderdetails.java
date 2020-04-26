2fe95f314c33a6e465832943c85b0c8b6120b82a
DrdroidAkshay <akshayyadav04031998@gmail.com>
2020-04-26 10:12:39 +0530

    RequestSent Completed

--- app/src/main/java/com/tripic/tripic/Riderdetails.java
+++ app/src/main/java/com/tripic/tripic/Riderdetails.java
@@ -1,5 +1,7 @@
 package com.tripic.tripic;
 
+import android.app.ProgressDialog;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -8,12 +10,25 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Riderdetails extends AppCompatActivity {
 
     TextView name,fromto,date,time;
     ImageView profilepic;
-    String profilename,profilefromto,profiledate,profiletime;
+    String profilename,profilefromto,profiledate,profiletime,username;
     Button sendrequestbtn;
+    String url;
+    String status,details;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +52,45 @@ public class Riderdetails extends AppCompatActivity {
         date.setText(profiledate);
         time.setText("Time: "+profiletime);
 
+        SharedPreferences sharedPreferences= getSharedPreferences("userdetails", 0);
+        username=sharedPreferences.getString("username","");
+        username=username.replace(" ","+");
+
         sendrequestbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(Riderdetails.this,"Request Sent",
-                        Toast.LENGTH_LONG).show();
+
+                url = "http://fullmoonfilms.000webhostapp.com/requeststatus.php?username1="+ username +"&username2="+profilename;
+                sendRequest();
             }
         });
+    }
+    private void sendRequest(){
+        ProgressDialog loading=ProgressDialog.show(Riderdetails.this,"Please wait",null,true,true);
+        StringRequest stringRequest=new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        loading.dismiss();
+                        try {
+                            JSONArray jsonArray=new JSONArray(response);
+                                JSONObject o=jsonArray.getJSONObject(0);
+                                      status=  o.getString("status");
+                                      details=  o.getString("details");
+                            Toast.makeText(Riderdetails.this,details,
+                                    Toast.LENGTH_LONG).show();
+                            } catch (JSONException ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
