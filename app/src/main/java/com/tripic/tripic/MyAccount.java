041c446379997f075709d777e62505419cae907c
DrdroidAkshay <akshayyadav04031998@gmail.com>
2020-04-28 19:23:27 +0530

    Name and phone shown in myaccount

--- app/src/main/java/com/tripic/tripic/MyAccount.java
+++ app/src/main/java/com/tripic/tripic/MyAccount.java
@@ -1,13 +1,17 @@
 package com.tripic.tripic;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.mikhaellopez.circularimageview.CircularImageView;
 
 public class MyAccount extends AppCompatActivity {
 
     CircularImageView profilepic,profilepicchangebtn;
+    TextView myaccountname,myaccountphone;
+    String name,phone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +20,14 @@ public class MyAccount extends AppCompatActivity {
 
         profilepic=findViewById(R.id.img_profile);
         profilepicchangebtn=findViewById(R.id.img_plus);
+        myaccountname=findViewById(R.id.myaccountname_id);
+        myaccountphone=findViewById(R.id.myaccountphone_id);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("userdetails", 0);
+        name = sharedPreferences.getString("username", "");
+        phone = sharedPreferences.getString("userphone", "");
+
+        myaccountname.setText(name);
+        myaccountphone.setText(phone);
     }
 }
