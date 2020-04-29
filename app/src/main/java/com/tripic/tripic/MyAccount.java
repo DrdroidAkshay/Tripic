6ca9a50f0783f34b01c9f3b63c6a0e314f69534f
DrdroidAkshay <akshayyadav04031998@gmail.com>
2020-04-29 18:51:17 +0530

    profile pic selection

--- app/src/main/java/com/tripic/tripic/MyAccount.java
+++ app/src/main/java/com/tripic/tripic/MyAccount.java
@@ -2,12 +2,15 @@ package com.tripic.tripic;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
 import com.mikhaellopez.circularimageview.CircularImageView;
+import com.theartofdev.edmodo.cropper.CropImage;
 
 public class MyAccount extends AppCompatActivity {
 
@@ -42,5 +45,25 @@ public class MyAccount extends AppCompatActivity {
                 startActivityForResult(galleryintent,gallerypic);
             }
         });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode==gallerypic && resultCode == RESULT_OK && data!=null){
+            Uri imageuri=data.getData();
+
+//            CropImage.activity()
+//                    .setGuidelines(CropImageView.Guidelines.ON)
+//                    .setAspectRatio(1,1)
+//                    .start(this);
+
+            CropImage.activity(imageuri)
+                    .setAspectRatio(1,1)
+                    .start(this);
+        }
+
     }
 }
