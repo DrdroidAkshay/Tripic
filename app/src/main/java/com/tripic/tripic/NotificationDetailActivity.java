2a94d5ec3d7b2501a1dfc51546a226146cd0b1e5
DrdroidAkshay <akshayyadav04031998@gmail.com>
2020-05-06 17:07:11 +0530

    Image shown in notification details

--- app/src/main/java/com/tripic/tripic/NotificationDetailActivity.java
+++ app/src/main/java/com/tripic/tripic/NotificationDetailActivity.java
@@ -16,6 +16,9 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.MemoryPolicy;
+import com.squareup.picasso.NetworkPolicy;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -24,79 +27,89 @@ import org.json.JSONObject;
 public class NotificationDetailActivity extends AppCompatActivity {
 
     ImageView profilepic;
-    TextView name,phone,requeststatus,contactmethod;
+    TextView name, phone, requeststatus, contactmethod;
     Button acceptrequestbtn, cancelrequestbtn;
-    String profilename,profilephone,status;
+    String profilename, profilephone, status;
     String accepturl, cancelurl;
-    String username,userphone;
-    String result,details;
+    String username, userphone;
+    String result, details;
+    String loadimageurl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_detail);
 
-        profilepic=findViewById(R.id.profilepic_id);
-        name=findViewById(R.id.profilename_id);
-        phone=findViewById(R.id.profilephone_id);
-        requeststatus=findViewById(R.id.requeststatus_id);
-        contactmethod=findViewById(R.id.contactmethod_id);
-        acceptrequestbtn=findViewById(R.id.acceptrequestbtn_id);
-        cancelrequestbtn=findViewById(R.id.cancelrequestbtn_id);
+        profilepic = findViewById(R.id.profilepic_id);
+        name = findViewById(R.id.profilename_id);
+        phone = findViewById(R.id.profilephone_id);
+        requeststatus = findViewById(R.id.requeststatus_id);
+        contactmethod = findViewById(R.id.contactmethod_id);
+        acceptrequestbtn = findViewById(R.id.acceptrequestbtn_id);
+        cancelrequestbtn = findViewById(R.id.cancelrequestbtn_id);
 
-        profilename=getIntent().getStringExtra("requestername");
-        profilephone=getIntent().getStringExtra("requesterphone");
-        status=getIntent().getStringExtra("requeststatus");
+        profilename = getIntent().getStringExtra("requestername");
+        profilephone = getIntent().getStringExtra("requesterphone");
+        status = getIntent().getStringExtra("requeststatus");
 
         SharedPreferences sharedPreferences = getSharedPreferences("userdetails", 0);
         username = sharedPreferences.getString("username", "");
         userphone = sharedPreferences.getString("userphone", "");
         username = username.replace(" ", "+");
         userphone = userphone.replace(" ", "+");
-
         name.setText(profilename);
         phone.setText(profilephone);
-        if (status.equals("accepted")){
+        if (status.equals("accepted")) {
             acceptrequestbtn.setVisibility(View.GONE);
             cancelrequestbtn.setVisibility(View.GONE);
-        }
-        else{
+        } else {
             requeststatus.setVisibility(View.GONE);
             contactmethod.setVisibility(View.GONE);
         }
+
+
         acceptrequestbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                profilename=profilename.replace(" ","+");
-                profilephone=profilephone.replace(" ","+");
-                accepturl="http://fullmoonfilms.000webhostapp.com/acceptrequest.php?username1="+ profilename+"&userphone1="+profilephone+"&username2="+username +"&userphone2="+userphone;
+                profilename = profilename.replace(" ", "+");
+                profilephone = profilephone.replace(" ", "+");
+                accepturl = "http://fullmoonfilms.000webhostapp.com/acceptrequest.php?username1=" + profilename + "&userphone1=" + profilephone + "&username2=" + username + "&userphone2=" + userphone;
                 acceptRequest();
             }
         });
         cancelrequestbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                profilename=profilename.replace(" ","+");
-                profilephone=profilephone.replace(" ","+");
-                cancelurl="http://fullmoonfilms.000webhostapp.com/cancelrequest.php?username1="+ profilename+"&userphone1="+profilephone+"&username2="+username +"&userphone2="+userphone;
+                profilename = profilename.replace(" ", "+");
+                profilephone = profilephone.replace(" ", "+");
+                cancelurl = "http://fullmoonfilms.000webhostapp.com/cancelrequest.php?username1=" + profilename + "&userphone1=" + profilephone + "&username2=" + username + "&userphone2=" + userphone;
                 cancelRequest();
+            }
+        });
+        profilepic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                profilename = profilename.replace(" ", "+");
+                loadimageurl = "http://fullmoonfilms.000webhostapp.com/Userprofilepics/" + profilename + ".jpeg";
+                loadimages(loadimageurl, profilepic);
             }
         });
 
     }
-    private void acceptRequest(){
-        ProgressDialog loading=ProgressDialog.show(NotificationDetailActivity.this,"Please wait",null,true,true);
-        StringRequest stringRequest=new StringRequest(Request.Method.GET, accepturl,
+
+    private void acceptRequest() {
+        ProgressDialog loading = ProgressDialog.show(NotificationDetailActivity.this, "Please wait", null, true, true);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, accepturl,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         loading.dismiss();
                         try {
-                            JSONArray jsonArray=new JSONArray(response);
-                            JSONObject o=jsonArray.getJSONObject(0);
-                            result=  o.getString("status");
-                            details=  o.getString("details");
-                            Toast.makeText(NotificationDetailActivity.this,details,
+                            JSONArray jsonArray = new JSONArray(response);
+                            JSONObject o = jsonArray.getJSONObject(0);
+                            result = o.getString("status");
+                            details = o.getString("details");
+                            Toast.makeText(NotificationDetailActivity.this, details,
                                     Toast.LENGTH_LONG).show();
                         } catch (JSONException ex) {
                             ex.printStackTrace();
@@ -109,23 +122,24 @@ public class NotificationDetailActivity extends AppCompatActivity {
 
                     }
                 });
-        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(stringRequest);
 
     }
-    private  void cancelRequest(){
-        ProgressDialog loading=ProgressDialog.show(NotificationDetailActivity.this,"Please wait",null,true,true);
-        StringRequest stringRequest=new StringRequest(Request.Method.GET, cancelurl,
+
+    private void cancelRequest() {
+        ProgressDialog loading = ProgressDialog.show(NotificationDetailActivity.this, "Please wait", null, true, true);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, cancelurl,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         loading.dismiss();
                         try {
-                            JSONArray jsonArray=new JSONArray(response);
-                            JSONObject o=jsonArray.getJSONObject(0);
-                            result=  o.getString("status");
-                            details=  o.getString("details");
-                            Toast.makeText(NotificationDetailActivity.this,details,
+                            JSONArray jsonArray = new JSONArray(response);
+                            JSONObject o = jsonArray.getJSONObject(0);
+                            result = o.getString("status");
+                            details = o.getString("details");
+                            Toast.makeText(NotificationDetailActivity.this, details,
                                     Toast.LENGTH_LONG).show();
                         } catch (JSONException ex) {
                             ex.printStackTrace();
@@ -138,8 +152,25 @@ public class NotificationDetailActivity extends AppCompatActivity {
 
                     }
                 });
-        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(stringRequest);
+
+    }
+
+    private void loadimages(String loadimageurl, ImageView profilepic) {
+
+        Picasso.with(NotificationDetailActivity.this).load(loadimageurl).memoryPolicy(MemoryPolicy.NO_CACHE).networkPolicy(NetworkPolicy.NO_CACHE).into(profilepic, new com.squareup.picasso.Callback() {
+            @Override
+            public void onSuccess() {
+
+            }
+
+            @Override
+            public void onError() {
+
+            }
+        });
+
 
     }
 }
